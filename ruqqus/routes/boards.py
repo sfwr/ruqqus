f703e2b94a1f924b76d8797a17e0b5cc1ad69d3b
captainmeta4 <adamladine@Adams-MacBook-Air.local>
2019-12-04 11:15:12 -0500

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -20,8 +20,8 @@ def api_board_available(name):
     else:
         return jsonify({"board":name, name:True})
 
-@app.route("/create_board", methods=["GET"])
-@app.route("/new_board", methods=["GET"])
+#@app.route("/create_board", methods=["GET"])
+@app.route("/make_board", methods=["GET"])
 @is_not_banned
 def create_board_get(v):
     return render_template("make_board.html", v=v)
