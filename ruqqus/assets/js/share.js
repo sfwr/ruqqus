681586096ce100049fa01b91ef239336dedb04b2
kek7198 <memezdank140@gmail.com>
2019-12-04 13:43:36 -0600

    Update share.js

--- ruqqus/assets/js/share.js
+++ ruqqus/assets/js/share.js
@@ -4,7 +4,7 @@ share_commentModal = function(url, snippet) {
 
 document.getElementById("share-modal-title").innerHTML = "Share comment";
 
-document.getElementById("twitter-share").href = "http://twitter.com/share?text=" + snippet + "&url=" url + "&hashtags=#MakeARuqqus";
+document.getElementById("twitter-share").href = "http://twitter.com/share?text=" + snippet + "&url=" + url + "&hashtags=#MakeARuqqus";
 
 document.getElementById("fb-share").href = "https://www.facebook.com/sharer/sharer.php?u=" + url + "&t=" + snippet;
 
@@ -12,7 +12,7 @@ document.getElementById("reddit-share").href = "http://www.reddit.com/submit?url
 
 document.getElementById("whatsapp-share").href = "https://api.whatsapp.com/send?text=" + url;
 
-document.getElementById("telegram-share").href = "https://telegram.me/share/url?url=" + url + "&text=" snippet;
+document.getElementById("telegram-share").href = "https://telegram.me/share/url?url=" + url + "&text=" + snippet;
 
 document.getElementById("sms-share").href = "sms://?body=" + url;
 
@@ -26,7 +26,7 @@ share_postModal = function(url, title) {
 
 document.getElementById("share-modal-title").innerHTML = "Share post";
 
-document.getElementById("twitter-share").href = "http://twitter.com/share?text=" + title + "&url=" url + "&hashtags=#MakeARuqqus";
+document.getElementById("twitter-share").href = "http://twitter.com/share?text=" + title + "&url=" + url + "&hashtags=#MakeARuqqus";
 
 document.getElementById("fb-share").href = "https://www.facebook.com/sharer/sharer.php?u=" + url + "&t=" + title;
 
@@ -34,7 +34,7 @@ document.getElementById("reddit-share").href = "http://www.reddit.com/submit?url
 
 document.getElementById("whatsapp-share").href = "https://api.whatsapp.com/send?text=" + url
 
-document.getElementById("telegram-share").href = "https://telegram.me/share/url?url=" + url + "&text=" snippet;
+document.getElementById("telegram-share").href = "https://telegram.me/share/url?url=" + url + "&text=" + snippet;
 
 document.getElementById("sms-share").href = "sms://?body=" + url;
 
