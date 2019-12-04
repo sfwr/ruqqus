15be50f5d123f2e72c38d5248d601d225bbcc5a0
captainmeta4 <adamladine@Adams-MacBook-Air.local>
2019-12-03 21:33:28 -0500

    Update submission.py

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -36,7 +36,7 @@ class Submission(Base):
     flags=relationship("Flag", lazy="dynamic", backref="submission")
     is_approved=Column(Integer, default=0)
     approved_utc=Column(Integer, default=0)
-    board_id=Column(Integer, default=None, ForeignKey("boards.id"))
+    board_id=Column(Integer, ForeignKey("boards.id"), default=None)
 
 
     #These are virtual properties handled as postgres functions server-side
