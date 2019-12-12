1cf1cda351d3197fbfc76d09d2ac1ab1bf81b298
captainmeta4 <captainmeta4@gmail.com>
2019-12-11 20:55:45 -0500

    fixed detached orm object

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -99,10 +99,13 @@ def board_name(name, v):
 
     sort=request.args.get("sort","hot")
     page=int(request.args.get("page", 1))
+
+    is_subscribed=(v and board.has_subscriber(v))
              
     return board.rendered_board_page(v=v,
                                      sort=sort,
-                                     page=page)
+                                     page=page,
+                                     is_subscribed=is_subscribed)
 
 @app.route("/mod/kick/<bid>/<pid>", methods=["POST"])
 @auth_required
