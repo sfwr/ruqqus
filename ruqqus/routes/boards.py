3096410e7f809686abd527fccc00532e82032405
captainmeta4 <captainmeta4@gmail.com>
2019-12-12 22:08:35 -0500

    Revert "fix modlist"
    This reverts commit 39e3e914d3b4ae6f6a8ffb126d28d7731ff72a42.

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -411,7 +411,9 @@ def board_about_mods(boardname, v):
     if not board:
         abort(404)
 
-    return render_template("guild/mods.html", v=v, b=board)
+    me=board.has_mod(v)
+
+    return render_template("guild/mods.html", v=v, b=board, me=me)
 
 
 @app.route("/+<boardname>/mod/exiled", methods=["GET"])
