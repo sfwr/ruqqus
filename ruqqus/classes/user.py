4bf80f5eb79b6c58ba00664a8ca917f45dd29d10
captainmeta4 <captainmeta4@gmail.com>
2019-12-18 16:20:50 -0500

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -360,7 +360,7 @@ class User(Base):
     @cache.memoize(timeout=60) 
     def comment_count(self):
 
-        return self.comments.filter(text("parent_submission is not null"))filter_by(is_banned=False, is_deleted=False).count()
+        return self.comments.filter(text("parent_submission is not null")).filter_by(is_banned=False, is_deleted=False).count()
 
     @property
     #@cache.memoize(timeout=60)
