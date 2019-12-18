22425044afc0f59d979371d4aefe7b2182f98efe
captainmeta4 <captainmeta4@gmail.com>
2019-12-18 10:39:23 -0500

    Update thumbs.py

--- ruqqus/helpers/thumbs.py
+++ ruqqus/helpers/thumbs.py
@@ -7,7 +7,7 @@ from ruqqus.__main__ import db
 
 def thumbnail_thread(post):
 
-    params={"access_key":enviorn.get("APIFLASH_KEY"),
+    params={"access_key":environ.get("APIFLASH_KEY"),
             "url": post.url,
             "height":720,
             "width":1280,
@@ -16,12 +16,10 @@ def thumbnail_thread(post):
             "thumbnail_width":300
             }
 
-    print("getting thumbnail")
+
     x=requests.get("https://api.apiflash.com/v1/urltoimage", params=params)
 
     post.thumb_id=urlparse(x.json()["url"]).path.split("/")[-1].split(".")[0]
-
-    print(post.thumb_id)
 
     db.add(post)
     db.commit()
