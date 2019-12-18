f819ddcba95047b8f6bffb26f0b808b652753060
captainmeta4 <captainmeta4@gmail.com>
2019-12-18 08:25:25 -0500

    Update thumbs.py

--- ruqqus/routes/thumbs.py
+++ ruqqus/routes/thumbs.py
@@ -7,7 +7,7 @@ from ruqqus.__.main__ import app, limiter
 APIFLASH_KEY=environ.get("APIFLASH_KEY")
 
 @app.route('/thumbs/<image_id>', methods=["GET"])
-def thumbs(<image_id>):
+def thumbs(image_id):
 
     url=f"https://api.apiflash.com/v1/urltoimage/cache/{image_id}.jpeg?access_key={APIFLASH_KEY}"
 
