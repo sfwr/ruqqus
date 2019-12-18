c1927be36ea31189a65865a3e2acd1869f70b74a
captainmeta4 <captainmeta4@gmail.com>
2019-12-18 09:51:58 -0500

    add thumbs helper

---
+++ ruqqus/helpers/thumbs.py
@@ -0,0 +1,31 @@
+import requests
+from os import environ
+from urllib.parse import urlparse
+import Threading
+
+from .get import *
+from ruqqus.__main__ import db
+
+def thumbnail_thread(post):
+
+    params={"access_key":enviorn.get("APIFLASH_KEY"),
+            "url": post.url,
+            "height":720,
+            "width":1280,
+            "format":"jpeg",
+            "response_type":"json",
+            "thumbnail_width":300
+            }
+
+    x=request.post("https://api.apiflash.com/v1/urltoimage", params=params)
+
+    post.thumb_id=urlparse(x.json()["url"]).split("/")[-1].split(".")[0]
+
+    db.add(post)
+    db.commit()
+
+
+def generate_thumbnail(post):
+
+    new_thread=threading.Thread(target=lambda:generate_thumbnail(post))
+    new_thread.start()
