48df08a57585a6270296218423e54a47dbcc15ab
captainmeta4 <captainmeta4@gmail.com>
2019-12-23 10:46:36 -0500

    Update legal.py

--- ruqqus/routes/legal.py
+++ ruqqus/routes/legal.py
@@ -40,7 +40,7 @@ def legal_final(v):
 
     send_mail(environ.get("admin_email"),
               "Legal request submission",
-              render_template_string("<div>{% for k in data %}<h1>{{ k }}<h1><p>{{ data[k] }}</p></div>",
+              render_template_string("<div>{% for k in data %}<h1>{{ k }}<h1><p>{{ data[k] }}</p>{% endfor %}</div>",
                                      data=data),
               files=request.files
               )
