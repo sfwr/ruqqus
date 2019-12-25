89e51982706574131a1c09edfd1c3b96cc450052
captainmeta4 <captainmeta4@gmail.com>
2019-12-25 18:44:08 -0500

    Update settings.py

--- ruqqus/routes/settings.py
+++ ruqqus/routes/settings.py
@@ -41,13 +41,15 @@ def settings_profile_post(v):
     if x==0:
         v.title_id=None
         updated=True
-    else:
+    elif x>0:
         title =get_title(x)
         if title.check_eligibility(v):
             user.title_id=title.id
             updated=True
         else:
             return render_template("settings_profile.html", v=v, error=f"Unable to set title {title.text} - {title.requirement_string}")
+    else:
+        abort(400)
         
     if updated:
         db.add(v)
