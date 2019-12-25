48143dea19b1c723c974f22a06582bf6b2bdc7bf
captainmeta4 <captainmeta4@gmail.com>
2019-12-25 17:39:43 -0500

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -28,7 +28,7 @@ class User(Base):
     over_18=Column(Boolean, default=False)
     creation_ip=Column(String, default=None)
     most_recent_ip=Column(String, default=None)
-    submissions=relationship("Submission", lazy="dynamic", backref="users")
+    submissions=relationship("Submission", lazy="dynamic", backref="users", primaryjoin="Submission.author_id==User.id")
     comments=relationship("Comment", lazy="dynamic", primaryjoin="Comment.author_id==User.id")
     votes=relationship("Vote", lazy="dynamic", backref="users")
     commentvotes=relationship("CommentVote", lazy="dynamic", backref="users")
