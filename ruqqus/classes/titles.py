292c939a50f48b067b3fcbadcb662927eb42f957
captainmeta4 <captainmeta4@gmail.com>
2019-12-26 08:35:24 -0500

    Update titles.py

--- ruqqus/classes/titles.py
+++ ruqqus/classes/titles.py
@@ -16,6 +16,7 @@ class Title(Base):
     qualification_expr = Column(String(256))
     requirement_string = Column(String(512))
     color=Column(String(6), default="888888")
+    kind=Column(Integer, default=1)
 
 
     def check_eligibility(self, v):
