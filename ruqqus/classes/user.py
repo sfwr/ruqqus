fb5508ea57381c98c86fb888c4d34f911cc6d36a
captainmeta4 <captainmeta4@gmail.com>
2019-12-29 08:41:24 -0500

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -472,5 +472,10 @@ class User(Base):
     @property
     def available_titles(self):
 
-        titles=[i for i in db.query(Title).order_by(text("id asc")).all() if eval(i.qualification_expr,{}, {"v":self, "Board":Board})]
+        locs={"v":self,
+              "Board":Board,
+              "Submission":Submission
+              }
+
+        titles=[i for i in db.query(Title).order_by(text("id asc")).all() if eval(i.qualification_expr,{}, locs)]
         return titles
