2e384f2e521bf00c0b19ce58412c3f14b7be5271
captainmeta4 <captainmeta4@gmail.com>
2019-12-31 22:01:05 -0500

    Update comments.py

--- ruqqus/routes/comments.py
+++ ruqqus/routes/comments.py
@@ -146,7 +146,7 @@ def api_comment(v):
     db.add(vote)
     db.commit()
 
-    return redirect(f"{c.permalink}?context=1")
+    return redirect(f"{c.permalink}?context=1#comment-{c.base36id}")
 
 
 @app.route("/edit_comment/<cid>", methods=["POST"])
