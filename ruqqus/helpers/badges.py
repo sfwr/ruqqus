911318c6ad93573e3c670ff0d8ba53c6982168a1
captainmeta4 <captainmeta4@gmail.com>
2020-01-03 10:20:58 -0500

    Update badges.py

--- ruqqus/helpers/badges.py
+++ ruqqus/helpers/badges.py
@@ -15,21 +15,23 @@ def badge_monitor():
         for user in db.query(User).filter_by(is_banned=False).all():
 
             for badge in badge_types:
-            if eval(badge.qualification_expr, {}, {'v':user}):
-                if not account.has_badge(badge.id):
-                    new_badge=Badge(user_id=user.id,
-                                    badge_id=badge.id,
-                                    created_utc=int(time.time())
-                                    )
-                    db.add(new_badge)
-                    db.commit()
-                    print(f"added {badge.name} to @{user.username}")
-            else:
-                bad_badge=user.has_badge(badge.id)
-                if bad_badge:
-                    db.delete(bad_badge)
-                    db.commit()
-                    print(f"removed {badge.name} from @{user.username}")
+                
+                if eval(badge.qualification_expr, {}, {'v':user}):
+                    
+                    if not account.has_badge(badge.id):
+                        new_badge=Badge(user_id=user.id,
+                                        badge_id=badge.id,
+                                        created_utc=int(time.time())
+                                        )
+                        db.add(new_badge)
+                        db.commit()
+                        print(f"added {badge.name} to @{user.username}")
+                else:
+                    bad_badge=user.has_badge(badge.id)
+                    if bad_badge:
+                        db.delete(bad_badge)
+                        db.commit()
+                        print(f"removed {badge.name} from @{user.username}")
 
         print("thread sleeping 1hr")
         time.sleep(3600)
