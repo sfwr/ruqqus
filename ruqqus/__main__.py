1e7d4f116dad369a11e119d04d4a320aced25310
captainmeta4 <captainmeta4@gmail.com>
2020-01-03 10:31:08 -0500

    fix

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -125,5 +125,41 @@ def after_request(response):
             
     return response
 
-    
+
+#start the badge monitoring thread
+
+def badge_monitor():
+
+    while True:
+
+        print("starting badge check thread")
+
+        badge_types=[x for x in db.query(BadgeDef).filter(BadgeDef.qualification_expr.isnot(None)).all()]
+
+        for user in db.query(User).filter_by(is_banned=False).all():
+
+            for badge in badge_types:
+                
+                if eval(badge.qualification_expr, {}, {'v':user}):
+                    
+                    if not user.has_badge(badge.id):
+                        new_badge=Badge(user_id=user.id,
+                                        badge_id=badge.id,
+                                        created_utc=int(time.time())
+                                        )
+                        db.add(new_badge)
+                        db.commit()
+                        print(f"added {badge.name} to @{user.username}")
+                else:
+                    bad_badge=user.has_badge(badge.id)
+                    if bad_badge:
+                        db.delete(bad_badge)
+                        db.commit()
+                        print(f"removed {badge.name} from @{user.username}")
+
+        print("thread sleeping 1hr")
+        time.sleep(3600)
+
+badge_thread=threading.Thread(target=badge_monitor)
+badge_thread.start()
     
