5609afbb6e2421773f82cb198274b5285a1144b9
captainmeta4 <captainmeta4@gmail.com>
2020-01-07 16:47:40 -0500

    Update board_relationships.py

--- ruqqus/classes/board_relationships.py
+++ ruqqus/classes/board_relationships.py
@@ -50,6 +50,27 @@ class BanRelationship(Base):
     def __repr__(self):
         return f"<Ban(id={self.id}, uid={self.uid}, board_id={self.board_id})>"
 
+class ContributorRelationship(Base):
+
+    __tablename__="contributors"
+    id = Column(BigInteger, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    board_id = Column(Integer, ForeignKey("boards.id"))
+    created_utc = Column(BigInteger, default=0)
+
+    user=relationship("User", lazy="subquery")
+    board=relationship("Board", lazy="subquery")
+
+    def __init__(self, *args, **kwargs):
+        if "created_utc" not in kwargs:
+            kwargs["created_utc"] = int(time.time())
+
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return f"<Contributor(id={self.id}, uid={self.uid}, board_id={self.board_id})>"
+
+
 class PostRelationship(Base):
 
     __tablename__="postrels"
