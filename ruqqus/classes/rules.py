3f625ad7d763c16d9337497b58681044c3d57545
arkit3kt <exodust@protonmail.com>
2020-01-07 00:54:15 -0500

    add rules class

---
+++ ruqqus/classes/rules.py
@@ -0,0 +1,19 @@
+from .mix_ins import *
+from flask import *
+import time
+from sqlalchemy import *
+from sqlalchemy.orm import relationship, deferred
+from ruqqus.helpers.base36 import *
+from ruqqus.helpers.lazy import lazy
+from ruqqus.__main__ import Base, db, cache
+
+class Rules(Base, Stndrd):
+
+    __tablename__="rules"
+    id = Column(BigInteger, primary_key=True)
+    board_id = Column(Integer, ForeignKey("boards.id"))
+    rule_body = Column(String(256))
+    rule_html = Column(String)
+
+    def __repr__(self):
+        return f"<Rule(id={self.id}, board_id={self.board_id})>"
