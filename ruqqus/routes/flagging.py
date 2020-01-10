0af2b404e763c717ec331fc148760d032d4ef074
captainmeta4 <captainmeta4@gmail.com>
2020-01-10 07:39:06 -0500

    Update flagging.py

--- ruqqus/routes/flagging.py
+++ ruqqus/routes/flagging.py
@@ -12,10 +12,32 @@ def api_flag_post(pid, v):
 
     post=get_post(pid)
 
-    flag=Flag(post_id=post.id,
-              user_id=v.id,
-              created_utc=int(time.time())
-              )
+    kind = request.form.get("report_type")
+    
+    if kind=="admin":
+        existing=db.query(Flag).filter_by(user_id=v.id, post_id=pid).first()
+
+        if existing:
+            return "",409
+
+        flag=Flag(post_id=post.id,
+                  user_id=v.id,
+                  created_utc=int(time.time())
+                  )
+        
+    elif kind=="guild"
+        existing=db.query(Report).filter_by(user_id=v.id, post_id=pid).first()
+
+        if existing:
+            return "",409
+
+        flag=Report(post_id=pid,
+                  user_id=v.id,
+                  created_utc=int(time.time())
+                  )
+    else:
+        return "",422
+        
 
     db.add(flag)
 
@@ -32,7 +54,7 @@ def api_flag_comment(cid, v):
     existing=db.query(CommentFlag).filter_by(user_id=v.id, comment_id=cid).first()
 
     if existing:
-        abort(409)
+        return "",409
 
     flag=CommentFlag(comment_id=cid,
               user_id=v.id,
@@ -44,23 +66,3 @@ def api_flag_comment(cid, v):
     db.commit()
     return "", 204
 
-@app.route("/api/report/post/<pid>", methods=["POST"])
-@is_not_banned
-def api_report_post(pid, v):
-
-    pid=base36decode(pid)
-
-    existing=db.query(Report).filter_by(user_id=v.id, post_id=pid).first()
-
-    if existing:
-        abort(409)
-
-    report=Report(post_id=pid,
-              user_id=v.id,
-              created_utc=int(time.time())
-              )
-
-    db.add(report)
-
-    db.commit()
-    return "", 204
