db534d7896695e7827b5f163b5a8152804531110
captainmeta4 <captainmeta4@gmail.com>
2020-01-15 10:43:09 -0500

    Update recomputes.py

--- scripts/recomputes.py
+++ scripts/recomputes.py
@@ -1,14 +1,14 @@
 import time
 import threading
 
-from ruqqus.classes import *
+from ruqqus.classes.submission import *
 from ruqqus.__main__ import db
 
 def recompute():
 
     while True:
 
-        for post in db.query(Submissions).filter(is_banned=False, is_deleted=False).all():
+        for post in db.query(Submission).filter(is_banned=False, is_deleted=False).all():
 
             post.score_hot = post.rank_hot
             post.score_disputed=post.rank_fiery
