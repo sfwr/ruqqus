343cd97a78d143826457ded50cf979649a6ebfc1
captainmeta4 <captainmeta4@gmail.com>
2020-01-15 11:16:15 -0500

    Update recomputes.py

--- scripts/recomputes.py
+++ scripts/recomputes.py
@@ -3,11 +3,13 @@ import threading
 
 from ruqqus.__main__ import db
 
+from ruqqus import classes
+
 def recompute():
 
     while True:
 
-        for post in db.query(ruqqus.classes.submission.Submission).filter(is_banned=False, is_deleted=False).all():
+        for post in db.query(classes.submission.Submission).filter(is_banned=False, is_deleted=False).all():
 
             post.score_hot = post.rank_hot
             post.score_disputed=post.rank_fiery
