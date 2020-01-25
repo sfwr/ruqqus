e95e7eda068b1f553c95c37874f73975b7b5a185
captainmeta4 <captainmeta4@gmail.com>
2020-01-25 17:17:49 -0500

    Update comment.py

--- ruqqus/classes/comment.py
+++ ruqqus/classes/comment.py
@@ -43,7 +43,7 @@ class Comment(Base, Age_times, Scores, Stndrd):
     #These are virtual properties handled as postgres functions server-side
     #There is no difference to SQLAlchemy, but they cannot be written to
     ups = deferred(Column(Integer, server_default=FetchedValue()))
-    downs=(Column(Integer, server_default=FetchedValue()))
+    downs=deferred(Column(Integer, server_default=FetchedValue()))
     age=Column(Integer, server_default=FetchedValue())
     is_public=Column(Boolean, server_default=FetchedValue())
 
