d030e7ec3817874b157271ccf8c270d7a866e0f0
captainmeta4 <captainmeta4@gmail.com>
2020-01-25 10:40:05 -0500

    Update recomputes.py

--- scripts/recomputes.py
+++ scripts/recomputes.py
@@ -11,15 +11,24 @@ def recompute():
 
         db.begin(subtransactions=True)
 
+        now=int(time.time())
+
+        cutoff=now-(60860*24*180)
+
         print("Beginning score recompute")
 
-        total=db.query(classes.submission.Submission).filter_by(is_banned=False, is_deleted=False).count()
+        total=db.query(classes.submission.Submission
+                       ).filter_by(is_banned=False, is_deleted=False
+                                   ).filter(classes.submission.Submission.created_utc>cutoff
+                                            ).count()
 
         print(f"{total} submissions to score")
 
         i=0
-        for post in db.query(classes.submission.Submission).filter_by(is_banned=False, is_deleted=False).order_by(classes.submission.Submission.id).all():
-
+        for post in db.query(classes.submission.Submission
+                       ).filter_by(is_banned=False, is_deleted=False
+                                   ).filter(classes.submission.Submission.created_utc>cutoff
+                                            ).all():
             i+=1
 
             post.score_hot = post.rank_hot
@@ -30,11 +39,11 @@ def recompute():
             db.add(post)
             db.commit()
 
-            print(f"{i}/{total} - {post.base36id}")
+            #print(f"{i}/{total} - {post.base36id}")
 
         print("Done. Sleeping 10min")
 
-        time.sleep(60)
+        time.sleep(600)
 
 
 recompute()
