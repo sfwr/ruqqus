70c30ce4fcd548e8cdcb150e5c3e2ae36d6665ad
kek7198 <memezdank140@gmail.com>
2020-01-31 10:52:39 -0600

    remove alignment

--- ruqqus/helpers/markdown.py
+++ ruqqus/helpers/markdown.py
@@ -37,7 +37,7 @@ class CustomRenderer(HTMLRenderer):
         if not user or user.reserved:
             return f"{space}@{target}"
         
-        return f'{space}<a href="{user.permalink}"><img src="/@{user.username}/pic/profile" class="profile-pic-20 align-top mr-1">@{user.username}</a>'
+        return f'{space}<a href="{user.permalink}"><img src="/@{user.username}/pic/profile" class="profile-pic-20 mr-1">@{user.username}</a>'
 
     def render_board_mention(self, token):
         space=token.target[0]
