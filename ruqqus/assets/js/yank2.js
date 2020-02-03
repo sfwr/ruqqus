2c88dde2a9478cffe4fcad82b5692e7e108cfff2
Adam LaDine <captainmeta4@users.noreply.github.com>
2020-02-03 07:48:36 -0500

    Update yank2.js

--- ruqqus/assets/js/yank2.js
+++ ruqqus/assets/js/yank2.js
@@ -28,6 +28,7 @@ yank_postModal = function(id, author, comments, points, thumb, title, author_lin
     this.innerHTML='<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Yanking post';	
     this.disabled = true;	
     document.getElementById("yank-post-form").submit();
+    window.location.reload(true);
   }
 
 };
