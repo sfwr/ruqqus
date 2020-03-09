fab8f91126f09ab38d5a8da8080b26dc10845404
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-03-09 08:14:02 -0400

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -58,6 +58,7 @@ class User(Base, Stndrd):
     banner_nonce=Column(Integer, default=0)
     last_siege_utc=Column(Integer, default=0)
     mfa_secret=Column(String(16), default=None)
+    show_offensive=Column(String(16), default=True)
 
     moderates=relationship("ModRelationship", lazy="dynamic")
     banned_from=relationship("BanRelationship", lazy="dynamic", primaryjoin="BanRelationship.user_id==User.id")
