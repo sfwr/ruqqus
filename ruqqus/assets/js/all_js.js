c45f94efad84201edb92538d711127bea5d6a9b5
kek7198 <memezdank140@gmail.com>
2020-03-17 15:57:17 -0500

    improve js

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1299,19 +1299,7 @@ var text = document.getElementById("post-text");
 
 var button = document.getElementById("create_button");
 
-// Check validity of inputs
-
-if (url.length == 0) {
-  text.required = true;
-} else if (text.length == 0) {
-  url.required = true;
-}
-else {
-  text.required = false;
-  url.required = false;
-}
-
-// Disable submit button if invalid inputs
+// Validity check
 
 var isValidTitle = title.checkValidity();
 
@@ -1319,9 +1307,23 @@ var isValidURL = url.checkValidity();
 
 var isValidText = text.checkValidity();
 
+// Toggle reuqired attribute
+
+if (isValidURL) {
+  text.required = false;
+} else if (isValidText) {
+  url.required = false;
+}
+else {
+  text.required = true;
+  url.required = true;
+}
+
+// Disable submit button if invalid inputs
+
 if (isValidTitle && isValidURL) {
   button.disabled = false;
-} else if (isValidTitle && url.length == 0 && isValidText) {
+} else if (isValidTitle && isValidText) {
   button.disabled = false;
 } else {
   button.disabled = true;
