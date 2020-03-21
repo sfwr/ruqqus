c6dca313210b40fa457d9e0fefc41f1a08fdcd36
captainmeta4 <captainmeta4@gmail.com>
2020-03-21 09:27:37 -0400

    Update thumbs.py

--- ruqqus/helpers/thumbs.py
+++ ruqqus/helpers/thumbs.py
@@ -1,6 +1,7 @@
 import requests
 from os import environ
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 from .get import *
 from ruqqus.__main__ import db
@@ -26,21 +27,36 @@ def thumbnail_thread(pid):
 
             return
 
-    url=f"https://api.apiflash.com/v1/urltoimage"
-    params={'access_key':environ.get("APIFLASH_KEY"),
-            'format':'png',
-            'height':720,
-            'width':1280,
-            'response_type':'image',
-            'thumbnail_width':600,
-            'url': post.embed_url if post.embed_url else post.url,
-            'css':"iframe {display:none;}"
-            }
-    x=requests.get(url, params=params)
-    print("have thumb from apiflash")
+##    url=f"https://api.apiflash.com/v1/urltoimage"
+##    params={'access_key':environ.get("APIFLASH_KEY"),
+##            'format':'png',
+##            'height':720,
+##            'width':1280,
+##            'response_type':'image',
+##            'thumbnail_width':600,
+##            'url': post.embed_url if post.embed_url else post.url,
+##            'css':"iframe {display:none;}"
+##            }
+
+
+    x=requests.get(post.url)
+    
+    if x.status_code != 200 or x.headers["Content-Type"]!="text/html":
+        print('not html post')
+        return
+
+    soup=BeautifulSoup(x.content, 'html.parser')
+    img=soup.find('img')
+    if not img:
+        print('no image in doc')
+        return
+
+    
+    x=requests.get(img['src'], params=params)
+    print("have first image")
 
     if x.status_code!=200:
-        print('no thumb')
+        print('no image')
         return
 
     name=f"posts/{post.base36id}/thumb.png"
