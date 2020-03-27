506065635b331a495183053cfdf472568914cd81
kek7198 <memezdank140@gmail.com>
2020-03-27 17:27:56 -0500

    Update all_js.js

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1411,7 +1411,7 @@ function check_exile(boardid) {
     x.onreadystatechange = function() {
       if (x.readyState == 4 && x.status == 200) {
         if (JSON.parse(x.response)["is_banned"] == true) {
-          console.log(JSON.parse(x.response))
+          console.log(JSON.parse(x.response));
           exileError.textContent = "It looks like that user is already banned.";
         }
       } else if (x.readyState == 4 && x.status == 404) {
