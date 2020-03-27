f49fcfc10e45a6a5467465e4fd3ee4f86120ddb4
kek7198 <memezdank140@gmail.com>
2020-03-27 16:51:24 -0500

    Update all_js.js

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1411,16 +1411,16 @@ function check_exile(boardid) {
     x.onreadystatechange = function() {
       if (x.readyState == 4 && x.status == 200) {
         if (x.status == 200) {
-          if (JSON.parse(x.response)["is_banned"] == true) {
-            exileError.textContent = "It looks like that user is already banned.";
-          }
-        } else if (x.status == 404) {
-          exileError.textContent = "Whoops, it looks like that user does not exist (yet)...";
+        if (JSON.parse(x.response)["is_banned"] == "true") {
+          exileError.textContent = "It looks like that user is already banned.";
         }
+      } else if (x.status == 404) {
+          exileError.textContent = "Whoops, it looks like that user does not exist (yet)...";
       }
-      x.open("GET", "/mod/check_exile/"+boardid+"?username="+username+"&formkey="+formkey(), true);
-      x.onload=function(){console.log(JSON.parse(x.response));console.log(JSON.parse(x.response)["is_banned"])};
-      x.send()
     }
+    x.open("GET", "/mod/check_exile/"+boardid+"?username="+username+"&formkey="+formkey(), true);
+    x.onload=function(){console.log(JSON.parse(x.response));console.log(JSON.parse(x.response)["is_banned"])};
+    x.send()
   }
+}
 }
