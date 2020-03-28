e54dd4ff8eaa7eef2ba0bd1743f3397c090167ad
kek7198 <memezdank140@gmail.com>
2020-03-28 11:55:15 -0500

    Update all_js.js

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1406,19 +1406,21 @@ function check_exile(boardid) {
     x.withCredentials=true;
     x.onreadystatechange = function() {
       if (x.readyState == 4 && x.status == 200) {
-        console.log(JSON.parse(x.response));
-        if (JSON.parse(x.response)["can_ban"] == true) {
-          exileForm.submit();
-        } else {
-
-          $('#toast-exile-error').toast('dispose');
+      console.log(JSON.parse(x.response));
+        if (JSON.parse(x.response)["is_banned"] == true) {
           $('#toast-exile-error').toast('show');
-
-          exileError.textContent = JSON.parse(x.response)["error"];
+          exileError.textContent = "It looks like that user is already banned.";
+        } else {
+          exileForm.submit();
         }
+      } else if (x.readyState == 4 && x.status == 404) {
+          console.log("Error 404 - user does not exist");
+
+          $('#toast-exile-error').toast('show');
+          exileError.textContent = "Whoops, it looks like that user does not exist";
       }
-      x.open("GET", "/mod/check_exile/"+boardid+"?username="+username+"&formkey="+formkey(), true);
-      x.send()
     }
+    x.open("GET", "/mod/check_exile/"+boardid+"?username="+username+"&formkey="+formkey(), true);
+    x.send()
   }
 }
