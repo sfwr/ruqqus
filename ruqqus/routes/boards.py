011860724044ff178d70898ed0073f0310649a87
arkit3kt <exodust@protonmail.com>
2020-03-29 18:57:15 -0400

    remove username param

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -235,7 +235,7 @@ def mod_ban_bid_user(bid, board, v):
 @auth_required
 @is_guildmaster
 @validate_formkey
-def mod_unban_bid_user(bid, username, board, v):
+def mod_unban_bid_user(bid, board, v):
     print("values :", request.values)
     print("args : ", request.args)
     user=get_user(request.values.get("username"))
