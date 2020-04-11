6428cf9ee5ae567496322b8db338c7d6ed2f06c4
captainmeta4 <captainmeta4@gmail.com>
2020-04-11 11:30:53 -0400

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -968,25 +968,24 @@ def mod_approve_bid_user(bid, board, v):
     if not user:
         return jsonify({"error":"That user doesn't exist."}), 404
 
-    if board.has_ban(user):
-        return jsonify({"error":f"@{user.username} is exiled from +{board.name} and can't be approved."}), 409
+    if board.has_contributor(user):
+        return jsonify({"error":f"@{user.username} is exiled from +{board.name} and can't currently be approved."}), 409
 
     #check for an existing deactivated approval
-    existing_ban=db.query(BanRelationship).filter_by(user_id=user.id, board_id=board.id, is_active=False).first()
-    if existing_ban:
-        existing_ban.is_active=True
-        existing_ban.created_utc=int(time.time())
-        existing_ban.banning_mod_id=v.id
-        db.add(existing_ban)
+    existing_contrib=db.query(ContributorRelationship).filter_by(user_id=user.id, board_id=board.id, is_active=False).first()
+    if existing_contrib:
+        existing_contrib.is_active=True
+        existing_contrib.created_utc=int(time.time())
+        existing_contrib.banning_mod_id=v.id
+        db.add(existing_contrib)
     else:
-        new_ban=BanRelationship(user_id=user.id,
+        new_contrib=ContributorRelationship(user_id=user.id,
                                 board_id=board.id,
-                                banning_mod_id=v.id,
                                 is_active=True)
-        db.add(new_ban)
+        db.add(new_contrib)
 
 
-        text=f"You have been exiled from +{board.name}.\n\nNone of your existing posts or comments have been removed, however, you will not be able to make any new posts or comments in +{board.name}."
+        text=f"You have added as an approved contributor to +{board.name}."
         send_notification(user, text)
             
     db.commit()
