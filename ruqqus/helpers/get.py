66f2b6c81db0ed777a505c25efe91d3c63644212
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-05-07 07:42:41 -0400

    Update get.py

--- ruqqus/helpers/get.py
+++ ruqqus/helpers/get.py
@@ -37,7 +37,7 @@ def get_posts(pids, v=None):
     ids = [base36decode(x) for x in pids]
 
     if v:
-        vt=db.query(Vote).filter_by(user_id=v.id, submission_id.in_(ids)).subquery()
+        vt=db.query(Vote).filter(user_id==v.id, submission_id.in_(ids)).subquery()
 
 
         items= db.query(Submission, vt.c.vote_type).filter(Submission.id.in_(ids)).join(vt, isouter=True).all()
