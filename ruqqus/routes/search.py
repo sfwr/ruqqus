5a759d7d8776cf6d7a5c2e863f27ea023a16ecb5
captainmeta4 <captainmeta4@gmail.com>
2020-05-09 16:46:11 -0400

    Update search.py

--- ruqqus/routes/search.py
+++ ruqqus/routes/search.py
@@ -6,6 +6,34 @@ from sqlalchemy import *
 from flask import *
 from ruqqus.__main__ import app, db
 
+def searchlisting(q, v=None, page=1)
+
+    posts = db.query(Submission).filter(func.lower(Submission.title).contains(query.lower()))
+
+
+    if not (v and v.over_18):
+        posts=posts.filter_by(over_18=False)
+
+    if v and v.hide_offensive:
+        posts=posts.filter_by(is_offensive=False)
+
+    if not(v and v.admin_level>=3):
+        posts=posts.filter_by(is_deleted=False, is_banned=False)
+
+    if sort=="hot":
+        posts=posts.order_by(Submission.score_hot.desc())
+    elif sort=="new":
+        posts=posts.order_by(Submission.created_utc.desc())
+    elif sort=="fiery":
+        posts=posts.order_by(Submission.score_fiery.desc())
+    elif sort=="top":
+        posts=posts.order_by(Submission.score_top.desc())
+        
+    total=posts.count()
+    posts=[x for x in posts.offset(25*(page-1)).limit(26).all()]
+
+    return total, [x.id for x in posts]
+
 @app.route("/search", methods=["GET"])
 @auth_desired
 def search(v, search_type="posts"):
@@ -53,39 +81,19 @@ def search(v, search_type="posts"):
 
         #posts search
 
-        posts = db.query(Submission).filter(func.lower(Submission.title).contains(query.lower()))
-
-
-        if not (v and v.over_18):
-            posts=posts.filter_by(over_18=False)
-
-        if v and v.hide_offensive:
-            posts=posts.filter_by(is_offensive=False)
-
-        if not(v and v.admin_level>=3):
-            posts=posts.filter_by(is_deleted=False, is_banned=False)
-
-        if sort=="hot":
-            posts=posts.order_by(Submission.score_hot.desc())
-        elif sort=="new":
-            posts=posts.order_by(Submission.created_utc.desc())
-        elif sort=="fiery":
-            posts=posts.order_by(Submission.score_fiery.desc())
-        elif sort=="top":
-            posts=posts.order_by(Submission.score_top.desc())
-            
-        total=posts.count()
-        posts=[x for x in posts.offset(25*(page-1)).limit(26).all()]
+        total, ids = searchlisting(query, v=v, page=page)
         
-        next_exists=(len(posts)==26)
-        results=posts[0:25]
+        next_exists=(len(ids)==26)
+        ids=posts[0:25]
+
+        posts=get_posts(ids, v=v)
 
         return render_template("search.html",
                                v=v,
                                query=query,
                                total=total,
                                page=page,
-                               listing=results,
+                               listing=posts,
                                sort_method=sort,
                                next_exists=next_exists
                                )
