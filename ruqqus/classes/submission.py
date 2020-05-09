861af3130d7e2536afe41780c0223e1c1f7fd12f
captainmeta4 <captainmeta4@gmail.com>
2020-05-09 13:46:57 -0400

    Update submission.py

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -154,7 +154,7 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
 
         private=not self.is_public and not self.board.can_view(v)
 
-        (print(self._preloaded_comments)
+        print(self._preloaded_comments)
         if private and not self.author_id==v.id:
             abort(403)
         elif private:
